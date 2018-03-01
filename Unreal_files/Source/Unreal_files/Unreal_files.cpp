a81bd2982d2ce11bef3685648b04b33721b56cb7
Daniil Kostyuk <xdxnxkx@gmail.com>
2018-03-01 20:36:46 +0300

    Первый коммит проекта.

---
+++ Unreal_files/Source/Unreal_files/Unreal_files.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Unreal_files.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal_files, "Unreal_files" );
---
+++ Unreal_files/Source/Unreal_files/Unreal_filesGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Unreal_filesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_FILES_API AUnreal_filesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
